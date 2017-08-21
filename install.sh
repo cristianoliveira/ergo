7f417e538293207fabd69c3082f73ca6aec983f8
Cristian Oliveira <contato@cristianoliveira.com.br>
2017-08-21 23:44:56 +0200

    fix: Linux installation script

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 VERSION=`curl https://raw.githubusercontent.com/cristianoliveira/ergo/master/.version`
 
-cd /tmp
+cd /tmp/
 
-wget https://github.com/cristianoliveira/ergo/releases/download/$VERSION/ergo-$VERSION--linux-amd64.tar.gz
-tar -xf ergo-$VERSION-x86_64-unknown-linux-gnu.tar.gz
+wget https://github.com/cristianoliveira/ergo/releases/download/$VERSION/ergo-$VERSION-linux-amd64.tar.gz
+tar -xf ergo-$VERSION-linux-amd64.tar.gz
 sudo cp ergo /usr/local/bin
 
 echo "Application was installed in /usr/local/bin. To uninstall just do rm /usr/local/bin/ergo"
