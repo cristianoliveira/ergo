aae2a571ed65b46d48d200980bace1257a90db74
Cristian Oliveira <contato@cristianoliveira.com.br>
2017-09-06 19:23:02 +0200

    chore: add messages to linux install

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@ VERSION=`curl https://raw.githubusercontent.com/cristianoliveira/ergo/master/.ve
 
 cd /tmp/
 
+echo "Dowloading ergo-$VERSION from repo..."
 wget https://github.com/cristianoliveira/ergo/releases/download/$VERSION/ergo-$VERSION-linux-amd64.tar.gz
 tar -xf ergo-$VERSION-linux-amd64.tar.gz
+
+echo "It is going to copy the binary ./ergo into /usr/local/bin and may need sudo."
 sudo cp ergo /usr/local/bin
 
-echo "Application was installed in /usr/local/bin. To uninstall just do rm /usr/local/bin/ergo"
+echo "Application was installed inside /usr/local/bin. To uninstall just do rm /usr/local/bin/ergo"
