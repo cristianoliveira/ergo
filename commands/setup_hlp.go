4a21a7b3c9a78f81f13eaf9d3ad452217394d80d
Adrian Clepcea <adi_clepcea@yahoo.com>
2017-10-01 20:26:05 +0300

    Add windows setup suport

---
+++ commands/setup_hlp.go
@@ -0,0 +1,10 @@
+// +build linux darwin
+
+package commands
+
+//ShowInternetOptions is just a dummy for now 
+//this is only used for Windows
+func ShowInternetOptions() {}
+//InetRefresh is just a dummy. It only has functionality
+//in Windows
+func InetRefresh()         {}
