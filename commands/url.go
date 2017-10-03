8bbf846766f8639c3c2210fcee3ff937c1c0bdf8
Adrian Clepcea <clepcea@keysafetyinc.com>
2017-10-03 12:52:48 +0300

    Fix protocol prefix

--- commands/url.go
+++ commands/url.go
@@ -2,6 +2,8 @@ package commands
 
 import (
 	"fmt"
+	"regexp"
+
 	"github.com/cristianoliveira/ergo/proxy"
 )
 
@@ -11,8 +13,17 @@ import (
 func URL(name string, config *proxy.Config) {
 	for _, s := range config.Services {
 		if name == s.Name {
-			localUrl := `http://` + s.Name + config.Domain
-			fmt.Println(localUrl)
+
+			localURL := s.Name + config.Domain
+
+			//a protocol legth between 3 and 8 should fit all protocols
+			valid := regexp.MustCompile("^\\w{3,8}\\:\\/\\/.*$")
+
+			if !valid.MatchString(s.Name) {
+				localURL = `http://` + localURL
+			}
+
+			fmt.Println(localURL)
 		}
 	}
 }
