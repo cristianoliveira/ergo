9aa188ef0decc0344876b9bf724977fb9f2fb719
Christian Stadach <c.stadach@emetriq.com>
2017-10-24 22:59:55 +0200

    added test for invalid run command domain option

--- main_test.go
+++ main_test.go
@@ -127,6 +127,29 @@ func TestRunCommand(t *testing.T) {
 	})
 }
 
+// TestInvalidRunCommandOptions will cause the main function to return a non-zero exit code
+// so we need to do some wrapping to make the test not fail.
+// For more info check ou to uset: https://talks.golang.org/2014/testing.slide#23
+func TestInvalidRunCommandOptions(t *testing.T) {
+	t.Run("invalid run command options so it shows usage", func(tt *testing.T) {
+		if os.Getenv("TEST_INVALID_RUN_OPTION") == "1" {
+			args := []string{"ergo", "run", "-domain=foo"}
+			os.Args = args
+
+			main()
+			// Output: USAGE and exit with an error code
+		}
+
+		cmd := exec.Command(testExec, "-test.run=TestInvalidRunCommandOptions")
+		cmd.Env = append(os.Environ(), "TEST_INVALID_RUN_OPTION=1")
+		err := cmd.Run()
+		if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+			return
+		}
+		t.Fatalf("process ran with err %v, want exit status 1", err)
+	})
+}
+
 func TestAddCommand(t *testing.T) {
 	t.Run("it shows usage", func(tt *testing.T) {
 		args := []string{"ergo", "add"}
