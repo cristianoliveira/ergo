4c9b9c3cc44c545c229d6db0f5607899af2eeece
Cristian Oliveira <cristian.oliveiradarosa@barzahlen.de>
2017-11-12 10:37:55 +0100

    refactor: Fix identation and change test

--- main_test.go
+++ main_test.go
@@ -23,21 +23,13 @@ func TestMain(t *testing.T) {
 // For more info check ou to uset: https://talks.golang.org/2014/testing.slide#23
 func TestMissingCommand(t *testing.T) {
 	t.Run("missing a command so it shows usage", func(tt *testing.T) {
-		if os.Getenv("TEST_MISSING_COMMAND") == "1" {
-			args := []string{"ergo"}
-			os.Args = args
+		args := []string{"ergo"}
+		os.Args = args
 
-			main()
-			// Output: USAGE and exit with an error code
+		result := command()
+		if result != nil {
+			t.Errorf("Expected result to not be nil")
 		}
-
-		cmd := exec.Command(testExec, "-test.run=TestMissingCommand")
-		cmd.Env = append(os.Environ(), "TEST_MISSING_COMMAND=1")
-		err := cmd.Run()
-		if e, ok := err.(*exec.ExitError); ok && !e.Success() {
-			return
-		}
-		t.Fatalf("process ran with err %v, want exit status 1", err)
 	})
 }
 
@@ -131,7 +123,8 @@ func TestRunCommand(t *testing.T) {
 // so we need to do some wrapping to make the test not fail.
 // For more info check ou to uset: https://talks.golang.org/2014/testing.slide#23
 func TestInvalidRunCommandOptions(t *testing.T) {
-	t.Run("invalid run command options so it shows usage", func(tt *testing.T) {
+	t.Run("when running with a wrong domain", func(tt *testing.T) {
+
 		if os.Getenv("TEST_INVALID_RUN_OPTION") == "1" {
 			args := []string{"ergo", "run", "-domain=foo"}
 			os.Args = args
