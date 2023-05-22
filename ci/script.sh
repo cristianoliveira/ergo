0ddfaab79f5681472da3784c258aaa7846856084
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-05-22 22:26:13 +0200

    ci: allows build and upload artefacts on release (#133)
    * ci: build and upload artifacts on release
    
    * trigger ci
    
    * setup go
    
    * fixip
    
    * fix branch
    
    * run only on release
    
    * remove travis config
    
    * remove unused scripts

--- ci/script.sh
+++ ci/script.sh
@@ -1,9 +1,13 @@
-# `script` phase: you usually build, test and generate docs in this phase
-
+#!/usr/bin/env bash
 set -ex
 
 make all
-make coverage
+
+cp bin/ergo ergo
 
 # sanity check the file type
-file bin/ergo
+file ergo
+
+# release tarball will look like 'go-ergo-v1.2.3-x86_64-unknown-linux-gnu.tar.gz'
+ARTIFACT="ergo-${RELEASE_TAG:?"Missing release tag"}-${TARGET}.tar.gz"
+tar czf "$ARTIFACT" ergo
