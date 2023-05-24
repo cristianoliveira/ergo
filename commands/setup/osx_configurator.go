ce686d09106fac7444b82feac4f03689a6a12c2a
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-05-24 10:13:47 +0200

    fix: setup cmd check supported OSX versions
    Partially address https://github.com/cristianoliveira/ergo/issues/123

--- commands/setup/osx_configurator.go
+++ commands/setup/osx_configurator.go
@@ -1,14 +1,73 @@
 package setup
 
+import (
+	"fmt"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
 // OSXConfigurator implements Configurator for windows
 type OSXConfigurator struct{}
 
+const SUPPORTED_OSX_VERSION = 10 // up to Catalina
+
+func checkSupportedVersion() (bool, error) {
+	cmd := `sw_vers -productVersion`
+	output, err := exec.Command("/bin/sh", "-c", cmd).Output()
+	if err != nil {
+		return false, err
+	}
+
+	var majorVersionNumber int
+	outputString := string(output)
+	if strings.Contains(outputString, ".") {
+		majorVersion := strings.Split(string(output), ".")[0]
+		majorVersionNumber, err = strconv.Atoi(majorVersion)
+	} else {
+		majorVersionNumber, err = strconv.Atoi(outputString)
+	}
+
+	if err != nil {
+		return false, err
+	}
+
+	if majorVersionNumber >= SUPPORTED_OSX_VERSION {
+		fmt.Println("The ergo setup is not supported for the current osx version.")
+		return false, nil
+	}
+
+	return true, nil
+}
+
 // SetUp is responsible for setting up the ergo as proxy
 func (c *OSXConfigurator) SetUp(proxyURL string) error {
+	isSupported, err := checkSupportedVersion()
+	if err != nil {
+		fmt.Println("Error while checking the current osx version: ", err)
+		return err
+	}
+
+	if !isSupported {
+		fmt.Println("The ergo setup is not supported for the current osx version.")
+		return nil
+	}
+
 	return RunnerDefault.Run(`networksetup`, `-setautoproxyurl "Wi-Fi" "`+proxyURL+`"`)
 }
 
 // SetDown is responsible for remove the ergo as proxy
 func (c *OSXConfigurator) SetDown() error {
+	isSupported, err := checkSupportedVersion()
+	if err != nil {
+		fmt.Println("Error while checking the current osx version: ", err)
+		return err
+	}
+
+	if !isSupported {
+		fmt.Println("The ergo setup is not supported for the current osx version.")
+		return nil
+	}
+
 	return RunnerDefault.Run(`networksetup`, `-setautoproxyurl "Wi-Fi" ""`)
 }
